348a0866ef4236a85c1344b61b2126395c1f4ba2
randomusernamea <stars.a@outlook.com>
2023-02-06 20:32:56 -0300

    css and a typo

--- test/src/comps/AddPlayerForm.jsx
+++ test/src/comps/AddPlayerForm.jsx
@@ -86,42 +86,42 @@ function AddPlayerForm(){
 
 
     return (
-        <div>
-            <form onSubmit={onSubmitDeportista}>
-                <label htmlFor="id">id:</label>
-                <input onChange={onChangeValueId} value={id} required placeholder = "id" type ="text" name="id"/><br/>
-                <label htmlFor="nombre">Nombre:</label>
-                <input onChange={onChangeValueNombre} value={nombre} required placeholder = "Nombre"type ="text" name="nombre"/><br/>
-                <label htmlFor="especialidad">Especialidad:</label>
-                <input onChange={onChangeValueEspecialidad} value={especialidad} required placeholder = "Especialidad"type ="text" name="especalidad"/><br/>
-                <label htmlFor="edad">Edad:</label>
-                <input onChange={onChangeValueEdad} value={edad} required placeholder = "Edad"type ="text" name="edad"/><br/>
-                <label htmlFor="altura">Altura:</label>
-                <input onChange={onChangeValueAltura} value={altura} required placeholder = "Altura"type ="text" name="altura"/><br/>
-                <label htmlFor="peso">Peso:</label>
-                <input onChange={onChangeValuePeso} value={peso} required placeholder = "Peso"type ="text" name="peso"/><br/>
-                <label htmlFor="nacionalidad">Nacionalidad:</label>
-                <input onChange={onChangeValueNacionalidad} value={nacionalidad} required placeholder = "Nacionalidad"type ="text" name="nacionalidad"/><br/>
-                <label htmlFor="record_personal">Record Persoal:</label>
-                <input onChange={onChangeValueRecord} value={record} required placeholder = "Record Personal"type ="text" name="record_personal"/><br/>
-                <label htmlFor="descripcion">Descripcion:</label>
-                <input onChange={onChangeValueDescripcion} value={descripcion} required placeholder = "Descripcion"type ="text" name="descripcion"/><br/>
-                <label htmlFor="energia">Energia:</label>
-                <input onChange={onChangeValueEnergia} value={energia} required placeholder = "Energia"type ="text" name="energia"/><br/>
-                <label htmlFor="fuerza">Fuerza:</label>
-                <input onChange={onChangeValueFuerza} value={fuerza} required placeholder = "Fuerza"type ="text" name="fuerza"/><br/>
-                <label htmlFor="resistencia">Resistencia:</label>
-                <input onChange={onChangeValueResistencia} value={resistencia} required placeholder = "Resistencia"type ="text" name="resistencia"/><br/>
-                <label htmlFor="agilidad">Agilidad:</label>
-                <input onChange={onChangeValueAgilidad} value={agilidad} required placeholder = "Agilidad"type ="text" name="agilidad"/><br/>
-                <label htmlFor="lesiones">Lesiones:</label>
-                <input onChange={onChangeValueLesiones} value={lesiones} required placeholder = "Lesiones"type ="text" name="lesiones"/><br/>
-                <label htmlFor="dedicacion">Dedicacion:</label>
-                <input onChange={onChangeValueDedicacion} value={dedicacion} required placeholder = "Dedicacion"type ="text" name="dedicacion"/><br/>
-                <label htmlFor="profesionalsmo">Profesionalismo:</label>
-                <input onChange={onChangeValueProfesionalismo} value={profesionalismo} required placeholder = "Profesionalismo"type ="text" name="profesionalismo"/><br/>
-                <label htmlFor="image">Imagen:</label>
-                <input onChange={onChangeValueImagen} value={imagen} required placeholder = "Imagen"type ="text" name="image"/><br/>
+        <div className ="addPlayerForm">
+            <form className = "formSubmit"onSubmit={onSubmitDeportista}>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="id">id:</label>
+                <input className = "formInput" onChange={onChangeValueId} value={id} required placeholder = "id" type ="text" name="id"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="nombre">Nombre:</label>
+                <input className = "formInput" onChange={onChangeValueNombre} value={nombre} required placeholder = "Nombre"type ="text" name="nombre"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="especialidad">Especialidad:</label>
+                <input className = "formInput" onChange={onChangeValueEspecialidad} value={especialidad} required placeholder = "Especialidad"type ="text" name="especalidad"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="edad">Edad:</label>
+                <input className = "formInput" onChange={onChangeValueEdad} value={edad} required placeholder = "Edad"type ="text" name="edad"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="altura">Altura:</label>
+                <input className = "formInput" onChange={onChangeValueAltura} value={altura} required placeholder = "Altura"type ="text" name="altura"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="peso">Peso:</label>
+                <input className = "formInput" onChange={onChangeValuePeso} value={peso} required placeholder = "Peso"type ="text" name="peso"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="nacionalidad">Nacionalidad:</label>
+                <input className = "formInput" onChange={onChangeValueNacionalidad} value={nacionalidad} required placeholder = "Nacionalidad"type ="text" name="nacionalidad"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="record_personal">Record Personal:</label>
+                <input className = "formInput" onChange={onChangeValueRecord} value={record} required placeholder = "Record Personal"type ="text" name="record_personal"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="descripcion">Descripcion:</label>
+                <input className = "formInput" onChange={onChangeValueDescripcion} value={descripcion} required placeholder = "Descripcion"type ="text" name="descripcion"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="energia">Energia:</label>
+                <input className = "formInput" onChange={onChangeValueEnergia} value={energia} required placeholder = "Energia"type ="text" name="energia"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="fuerza">Fuerza:</label>
+                <input className = "formInput" onChange={onChangeValueFuerza} value={fuerza} required placeholder = "Fuerza"type ="text" name="fuerza"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="resistencia">Resistencia:</label>
+                <input className = "formInput" onChange={onChangeValueResistencia} value={resistencia} required placeholder = "Resistencia"type ="text" name="resistencia"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="agilidad">Agilidad:</label>
+                <input className = "formInput" onChange={onChangeValueAgilidad} value={agilidad} required placeholder = "Agilidad"type ="text" name="agilidad"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="lesiones">Lesiones:</label>
+                <input className = "formInput" onChange={onChangeValueLesiones} value={lesiones} required placeholder = "Lesiones"type ="text" name="lesiones"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="dedicacion">Dedicacion:</label>
+                <input className = "formInput" onChange={onChangeValueDedicacion} value={dedicacion} required placeholder = "Dedicacion"type ="text" name="dedicacion"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="profesionalsmo">Profesionalismo:</label>
+                <input className = "formInput" onChange={onChangeValueProfesionalismo} value={profesionalismo} required placeholder = "Profesionalismo"type ="text" name="profesionalismo"/></div>
+                <div className ="formSubmitDiv"><label className = "formLabel" htmlFor="image">Imagen:</label>
+                <input className = "formInput" onChange={onChangeValueImagen} value={imagen} required placeholder = "Imagen"type ="text" name="image"/></div>
                 <button id="btn-neon" type="submit">
                     Enviar Datos
                 </button>
