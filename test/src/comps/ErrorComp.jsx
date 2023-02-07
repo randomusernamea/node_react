a5ac80f6f61c021c662209932a0e529e5358cb29
randomusernamea <stars.a@outlook.com>
2023-02-06 21:22:05 -0300

    error comp redirect

--- test/src/comps/ErrorComp.jsx
+++ test/src/comps/ErrorComp.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import {useEffect} from 'react';
+import { useNavigate } from "react-router";
 
 function ErrorComp() {
+    const navigate = useNavigate();
+    useEffect(() => {
+        waitThenNavigate();
+    }, []);
+    const waitThenNavigate = () => {
+        setTimeout(() => {  navigate("/") }, 1000);
+    }
+
     return (
-        <div><h1>404 page not found</h1></div>
+        <div><h1>404 page not found</h1>
+        <p>Redirecting to home page...</p></div>
     )
 }
 
